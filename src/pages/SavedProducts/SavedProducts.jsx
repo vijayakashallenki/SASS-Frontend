d7f1e1be4e4fef9b1069aa5a31b5628222ed382e
vijayakash <83111657+vijayakashallenki@users.noreply.github.com>
2024-05-20 02:12:45 +0530

    Update SavedProducts.jsx

--- src/pages/SavedProducts/SavedProducts.jsx
+++ src/pages/SavedProducts/SavedProducts.jsx
@@ -1,12 +1,16 @@
-import React, {useEffect, useState} from "react";
-import {Link, useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
-import * as Yup from 'yup'
-import {useDispatch, useSelector} from 'react-redux'
-import {fetchAllProducts} from '../../redux/slices/products/productsSlices.js';
-import DateFormatter from '../../utils/dateFormate.js'
-import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai'
-import {saveProductAction, unsaveProductAction, userDetailsAction} from '../../redux/slices/users/usersSlices.js';
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllProducts } from "../../redux/slices/products/productsSlices.js";
+import DateFormatter from "../../utils/dateFormate.js";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import {
+  saveProductAction,
+  unsaveProductAction,
+  userDetailsAction,
+} from "../../redux/slices/users/usersSlices.js";
 
 const SavedProducts = () => {
   const dispatch = useDispatch();
@@ -16,19 +20,17 @@ const SavedProducts = () => {
 
   const gotUser = userDetails;
 
-  const savedProducts = gotUser?.saved?.map((product) => {
+  const savedProducts = user?.saved?.map((product) => {
     return product?._id;
   });
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [filter, setFilter] = useState("all");
-
-  const { loading, appErr, serverErr, aLLProductsGot } = useSelector(
-    (state) => state?.products
-  );
+  // const { loading, appErr, serverErr, aLLProductsGot } = useSelector(
+  //   (state) => state?.products
+  // );
 
   useEffect(() => {
     dispatch(fetchAllProducts());
+    dispatch(userDetailsAction());
   }, [dispatch]);
 
   const savedProducts1 = userDetails?.saved;
@@ -48,7 +50,6 @@ const SavedProducts = () => {
   const npage = Math.ceil(savedProducts1?.length / recordsPerPage);
   const records = savedProducts1?.slice(firstIndex, lastIndex);
   const recordsReversed = records?.reverse();
-
 
   const numbers = Array.from({ length: npage }, (_, index) => index + 1);
 
@@ -97,86 +98,100 @@ const SavedProducts = () => {
             {recordsReversed?.map((singleProd) => {
               return (
                 <>
-                  <div class="w-full px-4 md:w-1/2 lg:w-1/3 flex justify-between">
-                    <div class="mx-auto mb-10 max-w-[370px] bg-white  rounded-xl p-2">
-                      <div className="flex justify-between"></div>
-                      <div class="mb-8 overflow-hidden rounded-lg">
-                        <img
-                          src={singleProd.image1}
-                          alt="image1"
-                          class="w-full"
-                        />
-                      </div>
-                      <div className="px-2 pb-4">
-                        <div className="flex justify-between">
-                          {" "}
-                          <span class="bg-blue-400/90 mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white">
-                            <DateFormatter date={singleProd.createdAt} />
-                          </span>
-                          <span>
-                            {savedProducts?.includes(singleProd._id) ? (
-                              <AiFillHeart
-                                onClick={() => handleDislike(singleProd._id)}
-                                className="cursor-pointer text-red-500"
-                                size={30}
-                              />
-                            ) : (
-                              <AiOutlineHeart
-                                onClick={() => handleLike(singleProd._id)}
-                                className="cursor-pointer text-red-500"
-                                size={30}
-                              />
-                            )}
-                          </span>
+                  <div
+                    key={singleProd._id}
+                    class="w-full px-4 md:w-1/2 lg:w-1/3 flex justify-center"
+                  >
+                    <div class="mx-auto mb-10 w-full bg-white rounded-xl p-2 max-w-[370px] h-[700px]">
+                      <div className="flex flex-col justify-between h-full">
+                        <div class="mb-4 overflow-hidden rounded-lg h-[300px] bg-gray-100 flex items-center justify-center">
+                          {singleProd.image1 ? (
+                            <img
+                              src={singleProd.image1}
+                              alt="image1"
+                              class="w-full h-full object-contain"
+                            />
+                          ) : (
+                            <span class="text-gray-400">No Image</span>
+                          )}
                         </div>
-                        <h3>
+                        <div className="px-2 pb-4 flex-grow flex flex-col justify-between">
+                          <div>
+                            <div className="flex justify-between">
+                              <span class="bg-blue-400/90 mb-2 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white">
+                                <DateFormatter date={singleProd.createdAt} />
+                              </span>
+                              <span>
+                                <AiFillHeart
+                                  onClick={() => handleDislike(singleProd._id)}
+                                  className="cursor-pointer text-red-500"
+                                  size={30}
+                                />
+                              </span>
+                            </div>
+                            <h3>
+                              <Link
+                                to={`/product/${singleProd._id}`}
+                                class="text-black hover:text-gray-800 mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
+                              >
+                                {singleProd.title}
+                              </Link>
+                            </h3>
+                            <p class="text-body-color text-base mb-4">
+                              {singleProd.descriptionHero?.slice(0, 100)}
+                            </p>
+                          </div>
+                          <div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Price of Goods:</span>
+                              <span className="pr-2">
+                                {singleProd.priceOfGoods}$
+                              </span>
+                            </p>
+                            <div className="h-[1px] bg-gray-200 w-full"></div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Sell Price:</span>
+                              <span className="pr-2">
+                                {singleProd.sellPrice}$
+                              </span>
+                            </p>
+                            <div className="h-[1px] bg-gray-200 w-full"></div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Estimated Profit:</span>
+                              <span className="pr-2">
+                                {singleProd.sellPrice - singleProd.priceOfGoods}
+                                $
+                              </span>
+                            </p>
+                            <div className="h-[1px] bg-green-200 w-full"></div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Platform:</span>
+                              <span
+                                className={`pr-2 ${
+                                  singleProd.bestPlatform === "Tiktok"
+                                    ? "text-red-500"
+                                    : singleProd.bestPlatform === "Facebook"
+                                    ? "text-blue-600"
+                                    : singleProd.bestPlatform === "Google"
+                                    ? "text-orange-500"
+                                    : null
+                                }`}
+                              >
+                                {singleProd.bestPlatform}
+                              </span>
+                            </p>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Category:</span>
+                              <span className="pr-2 text-sky-900">
+                                {singleProd.category}
+                              </span>
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex flex-col justify-center">
                           <Link
                             to={`/product/${singleProd._id}`}
-                            class="text-black hover:text-gray-800 mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
-                          >
-                            {singleProd.title}
-                          </Link>
-                        </h3>
-                        <p class="text-body-color text-base">
-                          {singleProd.descriptionHero?.slice(0, 170)}
-                        </p>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Price of Goods:</p>{" "}
-                          <p className="pr-2">{singleProd.priceOfGoods}$</p>
-                        </p>
-                        <div className="h-[1px] block bg-gray-200 w-full"></div>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Sell Price:</p>{" "}
-                          <p className="pr-2">{singleProd.sellPrice}$</p>
-                        </p>
-                        <div className="h-[1px] block bg-gray-200 w-full"></div>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Estimated Profit:</p>{" "}
-                          <p className="pr-2">
-                            {singleProd.sellPrice - singleProd.priceOfGoods}$
-                          </p>
-                        </p>
-                        <div className="h-[1px] block bg-green-200 w-full"></div>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Platform:</p>{" "}
-                          <p
-                            className={`pr-2 ${
-                              singleProd.bestPlatform === "Tiktok"
-                                ? "text-red-500"
-                                : singleProd.bestPlatform === "Facebook"
-                                ? "text-blue-600"
-                                : singleProd.bestPlatform === "Google"
-                                ? "text-orange-500"
-                                : null
-                            }`}
-                          >
-                            {singleProd.bestPlatform}
-                          </p>
-                        </p>
-                        <div className="flex flex-col justify-">
-                          <Link
-                            to={`/product/${singleProd._id}`}
-                            class="bg-blue-500 sm:mt-10 items-center rounded-lg py-2 px-6 text-center text-white hover:bg-blue-600 "
+                            class="bg-blue-500 mt-4 items-center rounded-lg py-2 px-6 text-center text-white hover:bg-blue-600"
                           >
                             See More
                           </Link>
