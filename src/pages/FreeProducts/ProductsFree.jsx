60748e50a6fa5dc751033bdda82af1ce229fc03b
vijayakash <83111657+vijayakashallenki@users.noreply.github.com>
2024-05-20 02:12:07 +0530

    Update ProductsFree.jsx

--- src/pages/FreeProducts/ProductsFree.jsx
+++ src/pages/FreeProducts/ProductsFree.jsx
@@ -1,46 +1,50 @@
-import React,{useEffect, useState} from "react";
-import {Link, useNavigate} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {fetchFreeProd} from '../../redux/slices/products/productsSlices.js'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFreeProd } from "../../redux/slices/products/productsSlices.js";
 import DateFormatter from "../../utils/dateFormate.js";
 
 const ProductsFree = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const {user} = useSelector((state)=> state?.users)
-  const {aLLFreeProductsGot} = useSelector((state)=>state?.products)
+  const { user } = useSelector((state) => state?.users);
+  const { aLLFreeProductsGot } = useSelector((state) => state?.products);
+
   useEffect(() => {
     dispatch(fetchFreeProd());
   }, [dispatch]);
+
   //pagnation variables
   const [currentPage, setCurrentPage] = useState(1);
   const recordsPerPage = 6;
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
-  console.log(aLLFreeProductsGot);
+  // console.log(aLLFreeProductsGot);
 
-  const npage = Math.ceil(aLLFreeProductsGot?.slice(firstIndex, lastIndex));
+  const npage = Math.ceil(aLLFreeProductsGot?.length / recordsPerPage);
   const records = aLLFreeProductsGot?.slice(firstIndex, lastIndex);
-  const numbers = Array.from({length: npage}, (_, index)=> index + 1);
+  const numbers = Array.from({ length: npage }, (_, index) => index + 1);
 
-
-
-  const prePage = ()=> {
-    if(currentPage !== firstIndex) {
+  const prePage = () => {
+    if (currentPage !== 1) {
       setCurrentPage(currentPage - 1);
+    } else {
+      setCurrentPage(1);
     }
   };
 
   const changeCPage = (id) => {
-    setCurrentPage(id)
-  }
+    setCurrentPage(id);
+  };
 
-  const nextPage = ()=> {
-    if(currentPage !== lastIndex) {
-      setCurrentPage(changeCPage + 1)
+  const nextPage = () => {
+    if (currentPage !== npage) {
+      setCurrentPage(currentPage + 1);
+    } else {
+      setCurrentPage(npage);
     }
-  }
+  };
 
   return (
     <div>
@@ -58,76 +62,81 @@ const ProductsFree = () => {
               </div>
             </div>
           </div>
-          <div>
-          </div>
+          <div></div>
           <div class="-mx-4 flex flex-wrap">
             {/* {map the records */}
-            {records?.map((singleProd)=> {
+            {records?.map((singleProd) => {
               return (
                 <>
-                  <div class="w-full px-4 md:w-1/2 lg:w-1/3 flex justify-between">
-                    <div class="mx-auto mb-10 max-w-[370px] bg-white  rounded-xl p-2">
-                      <div className="flex justify-between"></div>
-                      <div class="mb-8 overflow-hidden rounded-lg">
-                        <img
-                          src={singleProd.image1}
-                          alt="image1"
-                          class="w-full"
-                        />
-                      </div>
-                      <div className="px-2 pb-4">
-                        <span class="bg-blue-400/90 mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white">
-                          <DateFormatter date={singleProd.createdAt} />
-                        </span>
-                        <h3>
+                  <div
+                    key={singleProd._id}
+                    class="w-full px-4 md:w-1/2 lg:w-1/3 flex justify-center"
+                  >
+                    <div class="mx-auto mb-10 w-full bg-white rounded-xl p-2 max-w-[370px] h-[700px]">
+                      <div className="flex flex-col justify-between h-full">
+                        <div class="mb-4 overflow-hidden rounded-lg h-[300px] bg-gray-100 flex items-center justify-center">
+                          {singleProd.image1 ? (
+                            <img
+                              src={singleProd.image1}
+                              alt="image1"
+                              class="w-full h-full object-contain"
+                            />
+                          ) : (
+                            <span class="text-gray-400">No Image</span>
+                          )}
+                        </div>
+                        <div className="px-2 pb-4 flex-grow flex flex-col justify-between">
+                          <div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Price of Goods:</span>
+                              <span className="pr-2">
+                                {singleProd.priceOfGoods}$
+                              </span>
+                            </p>
+                            <div className="h-[1px] bg-gray-200 w-full"></div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Sell Price:</span>
+                              <span className="pr-2">
+                                {singleProd.sellPrice}$
+                              </span>
+                            </p>
+                            <div className="h-[1px] bg-gray-200 w-full"></div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Estimated Profit:</span>
+                              <span className="pr-2">
+                                {singleProd.sellPrice - singleProd.priceOfGoods}
+                                $
+                              </span>
+                            </p>
+                            <div className="h-[1px] bg-green-200 w-full"></div>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Platform:</span>
+                              <span
+                                className={`pr-2 ${
+                                  singleProd.bestPlatform === "Tiktok"
+                                    ? "text-red-500"
+                                    : singleProd.bestPlatform === "Facebook"
+                                    ? "text-blue-600"
+                                    : singleProd.bestPlatform === "Google"
+                                    ? "text-orange-500"
+                                    : null
+                                }`}
+                              >
+                                {singleProd.bestPlatform}
+                              </span>
+                            </p>
+                            <p className="pt-2 font-semibold flex justify-between">
+                              <span className="pl-2">Category:</span>
+                              <span className="pr-2 text-sky-900">
+                                {singleProd.category}
+                              </span>
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex flex-col justify-center">
                           <Link
-                            to={`/productFree/${singleProd._id}`}
-                            class="text-black hover:text-gray-800 mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
-                          >
-                            {singleProd.title}
-                          </Link>
-                        </h3>
-                        <p class="text-body-color text-base">
-                          {singleProd.descriptionHero?.slice(0, 170)}
-                        </p>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Price of Goods:</p>{" "}
-                          <p className="pr-2">{singleProd.priceOfGoods}$</p>
-                        </p>
-                        <div className="h-[1px] block bg-gray-200 w-full"></div>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Sell Price:</p>{" "}
-                          <p className="pr-2">{singleProd.sellPrice}$</p>
-                        </p>
-                        <div className="h-[1px] block bg-gray-200 w-full"></div>
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Estimated Profit:</p>{" "}
-                          <p className="pr-2">
-                            {singleProd.sellPrice - singleProd.priceOfGoods}$
-                          </p>
-                        </p>
-                        <div className="h-[1px] block bg-green-200 w-full"></div>
-
-                        <p className="pt-4 font-semibold flex justify-between ">
-                          <p className="pl-2">Platform:</p>{" "}
-                          <p
-                            className={`pr-2 ${
-                              singleProd.bestPlatform === "Tiktok"
-                                ? "text-red-500"
-                                : singleProd.bestPlatform === "Facebook"
-                                ? "text-blue-600"
-                                : singleProd.bestPlatform === "Google"
-                                ? "text-orange-500"
-                                : null
-                            }`}
-                          >
-                            {singleProd.bestPlatform}
-                          </p>
-                        </p>
-                        <div className="flex flex-col justify-">
-                          <Link
-                            to={`/productFree/${singleProd._id}`}
-                            class="bg-blue-500 sm:mt-10 items-center rounded-lg py-2 px-6 text-center text-white hover:bg-blue-600 "
+                            to={`/product/${singleProd._id}`}
+                            class="bg-blue-500 mt-4 items-center rounded-lg py-2 px-6 text-center text-white hover:bg-blue-600"
                           >
                             See More
                           </Link>
@@ -151,7 +160,7 @@ const ProductsFree = () => {
               </Link>
             </li>
             {/* {map the numbers */}
-            {numbers.map((n, i)=> (
+            {numbers.map((n, i) => (
               <li
                 className={`text-3xl mx-2 ${
                   currentPage === n ? "text-blue-500" : "text-gray-700"
